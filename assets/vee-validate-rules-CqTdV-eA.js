128c34d9103c358ea9d7bfa44c9493226d8f6b6d
Jack Huang <jack842248@gmail.com>
2025-08-31 17:09:08 +0800

    update pages

---
+++ assets/vee-validate-rules-CqTdV-eA.js
@@ -0,0 +1,5 @@
+const f="/images/img-todo-bg.png";/**
+  * vee-validate v4.15.1
+  * (c) 2025 Abdelrahman Awad
+  * @license MIT
+  */function i(r){return!!(r==null||r===""||Array.isArray(r)&&r.length===0)}const t=/^(?!\.)(?!.*\.\.)([A-Z0-9_'+\-\.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9\-]*\.)+[A-Z]{2,}$/i,o=r=>i(r)?!0:Array.isArray(r)?r.every(n=>t.test(String(n))):t.test(String(r));function e(r){return r==null}function s(r){return Array.isArray(r)&&r.length===0}const A=r=>e(r)||s(r)||r===!1?!1:!!String(r).trim().length;export{f as _,o as e,A as r};
